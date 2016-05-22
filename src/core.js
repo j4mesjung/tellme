4582d7ce1e5d9174bafb12775eef8a1c54aef45f
BE4R <jamesofjungle@gmail.com>
2016-05-22 16:56:26 -0700

    next/win logic + tests

--- src/core.js
+++ src/core.js
@@ -4,15 +4,36 @@ export function setEntries(state, entries){
   return state.set('entries', List(entries));
 };
 
+function getWinners(vote){
+  if(!vote) return [];
+  const [a,b] = vote.get('pair');
+  // Returns the value found by following a path of keys or indices through nested Iterables.
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if      (aVotes > bVotes) return [a];
+  else if (aVotes < bVotes) return [b];
+  else                      return [a,b];
+}
+
 export function next(state){
-  const entries = state.get('entries');
-  // Returns a new Map resulting from merging the provided Iterables (or JS objects) into this Map
-  return state.merge({
-    // Returns a new Iterable of the same type which includes the first amount entries from this Iterable.
-    vote: Map({pair: entries.take(2)}),
-    // Returns a new Iterable of the same type which excludes the first amount entries from this Iterable.
-    entries: entries.skip(2)
-  });
+  // returns a new Entries list with winners concatted at the end
+  const entries = state.get('entries')
+                       .concat(getWinners(state.get('vote')));
+
+  // if there is only one entry left, that is the winner
+  if (entries.size === 1){
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
+    // Returns a new Map resulting from merging the provided Iterables (or JS objects) into this Map
+    return state.merge({
+      // Returns a new Iterable of the same type which includes the first amount entries from this Iterable.
+      vote: Map({pair: entries.take(2)}),
+      // Returns a new Iterable of the same type which excludes the first amount entries from this Iterable.
+      entries: entries.skip(2)
+    });
+  };
 };
 
 export function vote(state, entry){
