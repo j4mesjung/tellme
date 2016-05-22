1a07ef524f5f9b4f7e9777e39d062e4b9e54872c
BE4R <jamesofjungle@gmail.com>
2016-05-22 15:58:25 -0700

    next() + tests

--- src/core.js
+++ src/core.js
@@ -1,5 +1,16 @@
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 export function setEntries(state, entries){
   return state.set('entries', List(entries));
+};
+
+export function next(state){
+  const entries = state.get('entries');
+  // Returns a new Map resulting from merging the provided Iterables (or JS objects) into this Map
+  return state.merge({
+    // Returns a new Iterable of the same type which includes the first amount entries from this Iterable.
+    vote: Map({pair: entries.take(2)}),
+    // Returns a new Iterable of the same type which excludes the first amount entries from this Iterable.
+    entries: entries.skip(2)
+  });
 };
