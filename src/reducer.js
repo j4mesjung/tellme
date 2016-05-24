4f1fcbbfe4304d89063a3f2056f3f0b5bf628cbc
BE4R <jamesofjungle@gmail.com>
2016-05-23 21:16:38 -0700

    set up an initial state

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,10 @@
-import {setEntries, next, vote} from './core';
+import {setEntries, next, vote, INITIAL_STATE} from './core';
 
 // Reducer - a generic function that takes any kind of action - along with the current state - and invokes the core function that matches the action
 
 // the Reducer should delegate to one of the core functions based on the action
 // the Reducer should know how to unpack the additional arguments of each function from the action object
-export default function reducer(state, action){
+export default function reducer(state = INITIAL_STATE, action){
   switch(action.type){
     case 'SET_ENTRIES':
       return setEntries(state, action.entries);
